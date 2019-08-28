2ef3f22d3a61453ad53c1a42ca5a1e7ec78b6f44
Tobe O <thosakwe@gmail.com>
2019-08-27 20:46:33 -0400

    Ir stub

--- src/ir.ml
+++ src/ir.ml
@@ -1,4 +1,7 @@
-type t =
+type  t = func list
+and func = string * branch list
+and branch = string * (instruction list)
+and instruction =
   | ReturnVoid
   (* signed * size * value *)
   | ReturnInt of bool * int * int
